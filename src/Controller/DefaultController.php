b8b7b71be7bcf3c7d5d52a0ea49bd8d4ea91ef16
Malte Hübner <maltehuebner@gmx.org>
2019-03-11 21:39:36 +0100

    Speed up list with cache.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,20 +5,30 @@ namespace App\Controller;
 use App\CoordEnricher\CoordEnricherInterface;
 use App\StrikeListParser\StrikeListParserInterface;
 use JMS\Serializer\SerializerInterface;
+use Psr\SimpleCache\CacheInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    const CACHE_KEY = 'strike-list';
+    const CACHE_TTL = 3600;
+
     /**
      * @Route("/list")
      */
-    public function listAction(StrikeListParserInterface $strikeListParser, CoordEnricherInterface $coordEnricher, SerializerInterface $serializer): Response
+    public function listAction(CacheInterface $cache, StrikeListParserInterface $strikeListParser, CoordEnricherInterface $coordEnricher, SerializerInterface $serializer): Response
     {
+        if ($cache->has(self::CACHE_KEY)) {
+            return new Response($serializer->serialize($cache->get(self::CACHE_KEY), 'json'));
+        }
+        
         $eventList = $strikeListParser->parse();
 
         $eventList = $coordEnricher->loadCoords()->enrichEventList($eventList);
+
+        $cache->set(self::CACHE_KEY, $eventList, self::CACHE_TTL);
 
         return new Response($serializer->serialize($eventList, 'json'));
     }
